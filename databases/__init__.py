921f959a9d6b6a51d1dd4d0e08c976903e197d9b
OCEP kun <josephmanullang@gmail.com>
2025-08-05 01:47:10 +0700

    update

--- databases/__init__.py
+++ databases/__init__.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import sessionmaker
 import os
 
 # Ganti dengan koneksi PostgreSQL Anda
-DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:manullang2003@localhost:5432/match_cv")
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
