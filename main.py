53478f72d290042b2a49bd726dd1e9f870bc8367
OCEP kun <josephmanullang@gmail.com>
2025-08-05 19:27:32 +0700

    update

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ app = FastAPI()
 # === Middleware CORS ===
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["https://match-cv.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
